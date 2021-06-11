20801103a7cddc17e6dd199b8d2751ed969ab2fb
anjskj <45606242+anjskj@users.noreply.github.com>
2021-06-11 10:55:32 +0200

    Initial commit

---
+++ OdeToFood/OdeToFood.Data/Services/InMemoryRestaurantData.cs
@@ -0,0 +1,28 @@
+﻿using OdeToFood.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OdeToFood.Data.Services
+{
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        List<Restaurant> restaurants;
+
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant {Id=1, Name="Anjas Pizza", Cuisine=CuisineType.Italian},
+                new Restaurant {Id=2, Name="Kalles Korvar", Cuisine=CuisineType.German},
+                new Restaurant {Id=3, Name="Indiana Jones", Cuisine=CuisineType.Indian}
+
+            };
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return restaurants.OrderBy(r=>r.Name);
+        }
+    }
+}
